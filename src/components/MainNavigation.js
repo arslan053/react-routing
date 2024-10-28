a83b1e79deb11747fbc81c96efc6d3d9682297a3
Muhammad Arslan <arslan05342@gmail.com>
2024-10-28 14:50:41 +0500

    basic and nested routes are added and navigation is also done with react routing dom

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom"
+import classes from './MainNavigation.module.css'
+
+export default function MainNavigation(){
+  return (
+  <header className={classes.header}>
+    <nav>
+      <ul className={classes.list}>
+        <li>
+          <NavLink 
+          to="/"
+          className={({ isActive }) => isActive ? classes.active : undefined }
+          end //as NAvLInk check the currently active path start with that path then it make it active IDK about new versions
+          >
+            Home</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/products"
+          className={({ isActive }) => isActive ? classes.active : undefined }
+          >
+            Products</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  );
+}
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import MainNavigation from "../components/MainNavigation";
+
+export default function Error(){
+  return <>
+  <MainNavigation />
+  <main>
+    <h1>An Error occured</h1>
+    <p>Couldn't Find the page</p>
+  </main>
+  </>
+}
